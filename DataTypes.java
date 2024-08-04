ada4ecfa80d8e390fb529104a774edc5a510712a
i-aman0 <khan.aman902688@gmail.com>
2024-08-05 00:17:02 +0530

    First Commit

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int num1 = 5;
+        double num2 = num1; // implicit type conversion 
+        System.out.println(num2);
+
+        // the reverse however is not possible, because compiler cannot implicitly convert from larger to smaller type
+        // it will give error
+        // to achieve this, we need to do explicit conversion 
+
+        int num3 = (int)num2; // explicit conversion
+        System.out.println(num3);
+    }
+}
